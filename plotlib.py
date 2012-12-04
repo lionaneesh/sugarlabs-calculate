54b32999f1d80b62d3930044b47ac91b9780b572
Aneesh Dogra <lionaneesh@gmail.com>
2012-12-04 20:18:15 +0530

    Add my copyright info.

--- plotlib.py
+++ plotlib.py
@@ -1,4 +1,5 @@
 # plotlib.py, svg plot generator by Reinier Heeres <reinier@heeres.eu>
+# Copyright (C) 2012 Aneesh Dogra <lionaneesh@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
